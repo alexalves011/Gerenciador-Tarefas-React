d461097fc4a7b8c0c79e7b059fcad901761eb582
alexalves011 <alexalves.work@gmail.com>
2025-09-17 16:18:56 -0300

    aula 01 react

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import AddTasks from "./components/AddTasks";
+import Tasks from "./components/Tasks";
+
+function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar Programação",
+      description: "Estudar React Js",
+      isCompleted: true,
+    },
+    {
+      id: 2,
+      title: "Fazer Compras",
+      description: "Fazer compra do mês",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Estudar para prova",
+      description: "Assistir vídeos das aulas",
+      isCompleted: false,
+    },
+  ]);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+      return task;
+    });
+
+    setTasks(newTasks);
+  }
+
+  const onDeleteTaskClick = (taskId) => {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);
+  };
+
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciar tarefas
+        </h1>
+        <AddTasks />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteTaskClick={onDeleteTaskClick}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
